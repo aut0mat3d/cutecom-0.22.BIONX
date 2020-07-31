1d06580ffaf4a330916f7c5180e3a0432b588928
aut0mat3d <aut0mat3d@users.noreply.github.com>
2020-07-31 06:51:06 +0200

    First commit

---
+++ build/CMakeFiles/cutecom.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/main.cpp" "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/build/CMakeFiles/cutecom.dir/main.o"
+  "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/build/moc_qcppdialogimpl.cxx" "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/build/CMakeFiles/cutecom.dir/moc_qcppdialogimpl.o"
+  "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/qcppdialogimpl.cpp" "/home/raobereg/BionX/BIB1/Sniffing/cutecom/cutecom-0.22.BIONX/build/CMakeFiles/cutecom.dir/qcppdialogimpl.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT3_SUPPORT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/qt4/QtDesigner"
+  "/usr/include/qt4/QtDeclarative"
+  "/usr/include/qt4/QtScriptTools"
+  "/usr/include/qt4/QtDBus"
+  "/usr/include/qt4/QtXml"
+  "/usr/include/qt4/QtSql"
+  "/usr/include/qt4/QtNetwork"
+  "/usr/include/qt4/QtXmlPatterns"
+  "/usr/include/qt4/QtHelp"
+  "/usr/include/qt4/QtUiTools"
+  "/usr/include/qt4/QtTest"
+  "/usr/include/qt4/QtScript"
+  "/usr/include/qt4/QtSvg"
+  "/usr/include/qt4/Qt3Support"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/share/qt4/mkspecs/default"
+  "/usr/include/qt4"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
